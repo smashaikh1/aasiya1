50aadec14d41737095e1e0ced6488468cd3a7bf1
smashaikh1 <106754371+smashaikh1@users.noreply.github.com>
2022-06-05 10:47:43 +0530

    Add files via upload
    2 file uploaded in aasiya1 repository..on sunday...

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package smaproject2;
+
+public class HelloWorld
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.print("smaproject2....Shaikh mohammed");
+	}
+}
---
+++ Sample.java
@@ -0,0 +1,12 @@
+package smaproject2;
+
+public class Sample 
+{
+
+	public static void main(String[] args) {
+		System.out.print("smaproject2......sample updated on sunday....");
+
+
+	}
+
+}
